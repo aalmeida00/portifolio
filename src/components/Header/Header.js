8fdebf2ded8e79ac1e9949c617f3e90e8bd4f494
Andre Almeida <andre-silva78@hotmail.com>
2021-06-13 22:14:44 -0300

    feat: Menu section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,47 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px'}}>
+          <DiCssdeck size="3rem"/>
+          <Span>Andre Almeida</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projetos</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Tecnologias</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>Sobre</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/aalmeida00">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com/aalmeida00">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://instagram.com/andrx3">
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
