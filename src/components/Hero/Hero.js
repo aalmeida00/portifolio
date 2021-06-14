a2fc47dedc260f0d55b3c5b76a9c913791450084
Andre Almeida <andre-silva78@hotmail.com>
2021-06-13 22:32:13 -0300

    feat: Portifolio Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Bem vindo ao<br />
+        meu portifólio
+      </SectionTitle>
+      <SectionText>
+        Desenvolvedor, cansado e curioso.
+      </SectionText>
+    <Button onClick={() => window.location = 'mailto:andre@aalmeida.tech'}>Saiba mais</Button>
+    </LeftSection>
+
+  </Section>
 );
 
 export default Hero;
