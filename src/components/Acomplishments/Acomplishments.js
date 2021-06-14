1fbd82b7fc2d132c3692670cd9e5a4606e74ad3e
aalmeida00 <andre-silva78@hotmail.com>
2021-06-14 18:48:50 -0300

    feat: conquistas pessoais

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 30, text: 'Projetos feitos' },
+  { number: 100, text: 'Clientes satisfeitos' },
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>Conquistas pessoais</SectionTitle>
+    <Boxes>
+      {data.map((card, i) => (
+        <Box key={i}>
+          <BoxNum>+ {card.number}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
