cc106d9a2de5d01ac7f8e3086badfc23bc4b27b7
aalmeida00 <andre-silva78@hotmail.com>
2021-06-14 17:39:47 -0300

    feat: projects section with constants file changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,57 @@
 import React from 'react';
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from './ProjectsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projetos</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            a <Img src={image} />
+            <TitleContent>
+              <HeaderThree>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent>Tecnologias</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks target="_blank" href={visit}>
+                Visitar
+              </ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
