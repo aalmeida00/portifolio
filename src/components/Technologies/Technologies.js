5f64b1c53faecddeecaebc0a6528dd30ace19c4f
aalmeida00 <andre-silva78@hotmail.com>
2021-06-14 18:01:53 -0300

    feat: tech sections done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { DiWordpress, DiReact, DiTrello } from 'react-icons/di';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Tecnologias</SectionTitle>
+    <SectionText>
+      Trabalhei com diversas tecnologias no mundo do desenvolvimento web.
+      Conheça algumas
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experiencia com <br />
+            React.js, Vue, SASS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiWordpress size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experiencia com <br />
+            PHP, Node e MySQL
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiTrello size="3rem" />
+        <ListContainer>
+          <ListTitle>Gestão</ListTitle>
+          <ListParagraph>
+            Experiencia com <br />
+            Gestão de times agéis
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
