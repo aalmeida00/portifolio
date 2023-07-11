9d7a8f01c6570b7dc4b4c1501e69b70eece29241
Andre Almeida <andre-silva78@hotmail.com>
2023-07-10 21:07:52 -0300

    Add intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import Avatar from "@/components/Avatar";
 import Title from "@/components/Title";
+import Span from "@/components/Span";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex gap-8">
-        <Title tag="h2">
-          Andre<br /> 
-          Almeida
-        </Title>
-        <Avatar 
+    <main className="flex flex-wrap min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex gap-8 max-w-screen-lg">
+        <section className="flex gap-4 flex-col flex-1">
+          <Title tag="h2">
+            Andre<br />
+            Almeida
+          </Title>
+          <Span>A brief introduction about me</Span>
+
+          <p>
+            My name is Andre Almeida, i`m a Senior Software Engineer working currently at
+            <a href="https://foxbit.com.br/" target="_blank">Foxbit</a>. Mostly working as a
+            Champion Frontend Engineer, working on our core product and some side products like our International Exchange called <a href="https://pro.foxbit.com/" target="_blank">PRO</a>.
+          </p>
+        </section>
+
+        <Avatar
           src="/avatar.jpg"
           size="large"
         />
